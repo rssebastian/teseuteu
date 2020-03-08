898994eab0aaeb263037c74c3272c852fe518195
unknown <sebastiansryan@gmail.com>
2020-03-08 15:49:14 -0700

    creating restaurant header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>It's Working!</h1>
+      <Header />
     </div>
   );
 }
